fcf816ee7021e6a504d4fa1081adc2638ff0586b
lorenzo-neri <lorenzo21neri@gmail.com>
2023-09-05 17:06:17 +0200

    add function calculatingTime

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,3 +8,32 @@ Ogni secondo il nostro countdown dovrà scalare fino alle 9:30 di domani mattina
 let now = new Date();
 console.log(now);
 
+let endCountdown = new Date(now);
+
+calculatingTime()
+
+//funzione per calcolare il tempo rimanente fino alle 9:30 di domani mattina
+function calculatingTime() {
+    //aggiungo un giorno a oggi
+    endCountdown.setDate(now.getDate() + 1);
+    //setto l'orario
+    endCountdown.setHours(9, 30, 0, 0);
+    console.log(endCountdown);
+
+    //faccio la differenza tra la data di oggi e quella di domani
+    const remainingTime = endCountdown - now;
+    console.log(remainingTime); //in millisecondi
+    console.log(remainingTime / 1000); //in secondi
+
+    //secondi
+    const seconds = Math.floor((remainingTime % 60000) / 1000);
+    console.log(seconds);
+    //minuti
+    const minutes = Math.floor((remainingTime % 3600000) / 60000);
+    console.log(minutes);
+    //ore
+    const hours = Math.floor(remainingTime / 3600000);
+    console.log(hours);
+
+    return seconds, minutes, hours;
+}
